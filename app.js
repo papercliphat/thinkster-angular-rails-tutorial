2c48432ec9e2c1fff85d0f12fbdafe2aabffdbb7
papercliphat <porter.michael.james@gmail.com>
2016-05-27 18:50:46 -0700

    angular-routing

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}])
+
+
+
 	.factory('posts', [function(){
   		var o = {
     	posts: []
@@ -7,7 +24,7 @@ angular.module('flapperNews', [])
 		}])
 
 
-angular.module('flapperNews', [])
+
   .controller('MainCtrl', [
 	'$scope',
 	'posts',
