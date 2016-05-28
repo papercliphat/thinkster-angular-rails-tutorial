92bb9e5cb67d31222fc8018c374fc3d9be2dff19
papercliphat <porter.michael.james@gmail.com>
2016-05-27 18:34:48 -0700

    angular-services

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 angular.module('flapperNews', [])
+	.factory('posts', [function(){
+  		var o = {
+    	posts: []
+  		};
+  		return o;
+		}])
+
+
+angular.module('flapperNews', [])
   .controller('MainCtrl', [
-  	'$scope',
-  	function($scope){
+	'$scope',
+	'posts',
+		function($scope, posts){
   		$scope.test = 'Hello world!';
 
-  		 $scope.posts = [
- 			{title: 'post 1', upvotes: 5},
- 			{title: 'post 2', upvotes: 2},
- 			{title: 'post 3', upvotes: 15},
- 			{title: 'post 4', upvotes: 9},
- 			{title: 'post 5', upvotes: 4}
-		  ];
+  		 $scope.posts = posts.posts;
 
 		  $scope.addPost = function(){
  			 if(!$scope.title || $scope.title === '') { return; }
